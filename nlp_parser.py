516ebf9269bb8441d3c8a42ee477a29a3bb3b030
siddharth ghosh <siddharth.kido@gmail.com>
2025-05-31 18:05:58 -0700

    different priority handling, datetime model  for keywords

--- nlp_parser.py
+++ nlp_parser.py
@@ -5,12 +5,84 @@
 
 import spacy
 import task.task_class
+import dateparser
+from datetime import datetime, timedelta
 
 # spacy test
 # nlp = spacy.load("en_core_web_sm")
 # doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 # for token in doc:
 #     print(token.text, token.pos_, token.dep_)
+
+#CONFIG
+HIGH_PRIORITY = [
+    "exam",
+    "test",
+    "final",
+    "deadline",
+    "submit",
+    "turn in",
+    "presentation",
+    "project",
+    "application",
+    "app",
+    "interview",
+    "assignment",
+    "quiz",
+    "report",
+    "lab",
+    "registration",
+    "scholarship",
+    "paper",
+    "recommendation",
+    "competition",
+]
+
+MEDIUM_PRIORITY = [
+    "print",
+    "email",
+    "read",
+    "draft",
+    "meeting",
+    "study",
+    "revise",
+    "edit",
+    "club",
+    "outline",
+    "practice",
+    "notes",
+    "note",
+    "feedback",
+    "schedule",
+    "organize",
+    "brainstorm",
+    "homework",
+    "recording",
+    "reading",
+    "research",
+    "vocabulary",
+    "lecture"
+]
+LOW_PRIORITY = [
+    "clean",
+    "backpack",
+    "locker",
+    "decorate",
+    "supplies",
+    "pens",
+    "flashcards",
+    "organize",
+    "folders",
+    "binders",
+    "scan",
+    "back up",
+    "reminder",
+    "setup",
+    "checklist",
+    "prepare",
+    "download",
+    "calendar"
+]
 
 nlp  = spacy.load("en_core_web_sm")
 
@@ -26,4 +98,19 @@ def parse_text(text):
             task_deadline = ent.text
             task_name = text.replace(ent.text, "").strip()
 
-    
+        if task_deadline:
+            if "tomorrow" in task_deadline.lower():
+                deadline_date = (datetime.now() + timedelta(days=1)).strftime("%m/%d/%Y")
+
+            elif "today" in task_deadline.lower():
+                deadline_date = datetime.now().strftime("%m/%d/%Y")
+
+            elif "monday" in task_deadline.lower() or "tuesday" in task_deadline.lower() or "wednesday" in task_deadline.lower() or "thursday" in task_deadline.lower() or "friday" in task_deadline.lower() or "saturday" in task_deadline.lower() or "sunday" in task_deadline.lower():
+                deadline_date = dateparser.parse(task_deadline, settings={'PREFER_DATES_FROM': 'future'}).strftime("%m/%d/%Y")
+            else:
+                deadline_date = task_deadline
+        else:
+            deadline_date = None
+
+        
+    task_name = task_name.replace("by", "").strip()
