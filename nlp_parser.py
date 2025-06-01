fd76ec86e490441ca2f5049d3f851cbd7ea1261a
siddharth ghosh <siddharth.kido@gmail.com>
2025-05-31 17:16:44 -0700

    parse func defined, date time categorized

--- nlp_parser.py
+++ nlp_parser.py
@@ -6,8 +6,24 @@
 import spacy
 import task.task_class
 
+# spacy test
+# nlp = spacy.load("en_core_web_sm")
+# doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+# for token in doc:
+#     print(token.text, token.pos_, token.dep_)
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+nlp  = spacy.load("en_core_web_sm")
+
+def parse_text(text):
+    doc = nlp(text)
+
+    task_name = text
+    task_deadline = None
+    task_priority = ""
+
+    for ent in doc.ents:
+        if ent.label_ in ("DATE", "TIME"):
+            task_deadline = ent.text
+            task_name = text.replace(ent.text, "").strip()
+
+    
