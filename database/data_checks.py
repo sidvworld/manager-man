034aff5a71b625447d873afb3688e2bda51c0655
siddharth ghosh <siddharth.kido@gmail.com>
2025-05-31 20:24:34 -0700

    data checks

--- database/data_checks.py
+++ database/data_checks.py
@@ -1,12 +1,14 @@
 import sqlite3
 
-def see_data():
+def see_table(table):
     con = sqlite3.connect('database/tasks.db')
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM tasks_table")
+    cur.execute(f"SELECT * FROM {table}")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-    con.commit()
     con.close()
 
-see_data()
+see_table("tasks_table")
--- hotkey_listener.py
+++ hotkey_listener.py
@@ -1,8 +1,17 @@
 from ui.overlay import show_overlay
+from nlp_parser import parse_text
+from db_task import save_task, init_db
 import keyboard
 
+def handle_input(user_input):
+    if user_input:
+        task_obj = parse_text(user_input)
+        save_task(task_obj)
+        print(f"task saved: {task_obj.task_name}, {task_obj.task_deadline}, {task_obj.task_priority}, {task_obj.created_at}")
+
 def start_hotkey_listener(): 
-    keyboard.add_hotkey('alt+q', lambda: show_overlay(callback=lambda user_input: print(user_input)))
+    init_db()
+    keyboard.add_hotkey('alt+q', lambda: show_overlay(callback=handle_input), suppress=True)
     keyboard.wait('right alt+`')
 
 if __name__ == '__main__':
