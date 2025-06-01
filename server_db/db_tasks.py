72df7fc80303720fe64bf3f1feeaf744fb8d91cc
siddharth ghosh <siddharth.kido@gmail.com>
2025-06-01 15:58:56 -0700

    more error handling man

--- server_db/db_tasks.py
+++ server_db/db_tasks.py
@@ -10,7 +10,7 @@ def save_task_table(task_obj):
             "task_priority": task_obj.task_priority,
             "created_at": task_obj.created_at
         }
-        res = requests.post(f"http://10.0.1.159:8000/save_task_table/", json=payload)
+        res = requests.post("http://10.0.1.159:8000/save_tasks_table/", json=payload)
         res.raise_for_status()
         print(f"task saved: {task_obj.task_name}, {task_obj.task_deadline}, {task_obj.task_priority}, {task_obj.created_at}")
     except Exception as e:
