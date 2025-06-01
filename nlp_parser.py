47e60a70b4b4480dcd3d4d0c935ce07fab9dfebb
siddharth ghosh <siddharth.kido@gmail.com>
2025-05-31 17:02:20 -0700

    nlp test

--- nlp_parser.py
+++ nlp_parser.py
@@ -1,4 +1,13 @@
 #time for the big boy part.
 
-import spacy
+# https://spacy.io/usage/spacy-101
+# use tokenization and text classification to parse text
 
+import spacy
+import task.task_class
+
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
